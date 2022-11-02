61a07762e63a924676ec3dc3c402ce428c706b96
Silas Reichlin <thierry_loetscher@sluz.ch>
2022-11-02 13:44:41 +0100

    TDD Tasks, now correct account

--- Aufgaben/02-tdd/FooBarQix.Tests/FooBarQixDeterminerTest.cs
+++ Aufgaben/02-tdd/FooBarQix.Tests/FooBarQixDeterminerTest.cs
@@ -4,35 +4,9 @@ namespace FooBarQix.Tests
 {
     public class FooBarQixDeterminerTest
     {
-        [Theory]
-        [InlineData(1, "1")]
-        [InlineData(2, "2")]
-        [InlineData(4, "4")]
-        [InlineData(8, "8")]
-        public void Determine_NotDividableNumber_ReturnsNumber(int number, string expected)
+        [Fact]
+        public void Test1()
         {
-            var acutal = FooBarQixDeterminer.Determine(number);
-
-            Assert.Equal(expected, acutal);
-        }
-
-        [Theory]
-        [InlineData(3, "FooFoo")]
-        [InlineData(5, "BarBar")]
-        [InlineData(7, "QixQix")]
-        [InlineData(10, "Bar")]
-        [InlineData(13, "Foo")]
-        [InlineData(15, "FooBarBar")]
-        [InlineData(21, "FooQix")]
-        [InlineData(33, "FooFooFoo")]
-        [InlineData(51, "FooBar")]
-        [InlineData(53, "BarFoo")]
-        [InlineData(105, "FooBarQixBar")]
-        public void Determine_DiviableNumber_ReturnsCorrectString(int number, string expected)
-        {
-            var actual = FooBarQixDeterminer.Determine(number);
-
-            Assert.Equal(expected, actual);
         }
     }
 }
